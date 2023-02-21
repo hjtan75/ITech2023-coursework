6c7684902c280f70d8cf11ae34cb9da99f2a7cfb
Huapeng Xie <1812412957@qq.com>
2023-02-21 23:39:47 +0000

    add static and media file,add css and js files

---
+++ yars/static/js/showBooking-jquery.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $('.edit_booking').click(function() {
+        alert('You clicked the button using JQuery!');
+    });
+    $('p').hover(
+function() {
+        $(this).css('color', 'red');
+},
+function() {
+        $(this).css('color', 'black');
+});
+    $('.delete_booking').click(function() {
+        alert('You clicked the button using JQuery!');
+});
+
+
+});
+
+
+
--- yars/yars/urls.py
+++ yars/yars/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('tutti/', include('tutti.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
