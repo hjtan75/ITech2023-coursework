9301a57ca97b3cd3f5000bfaf127eadc58417314
Huapeng Xie <1812412957@qq.com>
2023-02-23 20:05:23 +0000

    delete booking done

--- yars/static/js/showBooking-jquery.js
+++ yars/static/js/showBooking-jquery.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     $('.edit_booking').click(function() {
         alert('You clicked the button using JQuery!');
+
     });
     $('p').hover(
 function() {
@@ -11,7 +12,22 @@ function() {
         $(this).css('color', 'black');
 });
     $('.delete_booking').click(function() {
-        alert('You clicked the button using JQuery!');
+
+        var bookingID;
+        var row = $(this).closest('tr');
+        bookingID = $(this).attr('data-bookingid');
+        alert(bookingID);
+        $.get('/tutti/delete_booking/',
+            {'booking_id': bookingID},
+            function(data) {
+
+                if (data.status == 'success') {
+                // remove the row from the table
+                row.remove();
+            }
+
+            })
+
 });
 
 
--- yars/tutti/views.py
+++ yars/tutti/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.contrib.auth.decorators import login_required
 from tutti.models import User, Booking
+from django.http import JsonResponse
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("Rango says hey there partner!")
 
 
+# View to show booking
 def show_bookings(request):
     # Create a context dictionary which we can pass
     # to the template rendering engine.
@@ -14,7 +21,7 @@ def show_bookings(request):
         # Can we find a booking with the given username?
         # If we can't, the .get() method raises a DoesNotExist exception.
         # The .get() method returns one model instance or raises an exception.
-        bookings = Booking.objects.filter(user_id=1)
+        bookings = Booking.objects.filter(user_id=8)
         print(bookings)
         # Retrieve all of the associated bookings.
         # Adds our results list to the template context under name pages.
@@ -28,3 +35,21 @@ def show_bookings(request):
 
     # Go render the response and return it to the client.
     return render(request, 'tutti/show_bookings.html', context=context_dict)
+
+
+# View to delete booking.
+class DeleteBookingView(View):
+
+    def get(self, request):
+        booking_id = request.GET['booking_id']
+        try:
+            bookings = Booking.objects.get(bookingID=booking_id)
+            print(bookings)
+        except bookings.DoesNotExist:
+            return HttpResponse(-1)
+        except ValueError:
+            return HttpResponse(-1)
+        bookings.delete()
+
+        return JsonResponse({'status': 'success'})
+
