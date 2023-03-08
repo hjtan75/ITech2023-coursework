cf53b81a61c398465217f9fca1faf5b1b3ab70d5
redchicken75 <flyingnyancat7@gmail.com>
2023-03-08 21:43:45 +0000

    Added function that returns a number of seats left given a datetime

--- yars/booking_function.py
+++ yars/booking_function.py
@@ -5,21 +5,22 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yars.settings')
 
 
 import django
+from django.db.models import Sum
 django.setup()
 from tutti.models import User, Booking
 
-numOfPeoplePerSlow = 30
+numOfPeoplePerSlot = 30
 
 def numSeatsForDate(date_string):
     # Return the number of seats left for a given date
     # Date is string with format yy-mm-dd
     dt = datetime.strptime(date_string, '%Y-%m-%d').date()
-    booking = Booking.objects.filter(date=dt)
-    print(booking)
-
+    nop = Booking.objects.filter(date=dt).aggregate(Sum('numberOfPeople'))
+    seats_left = numOfPeoplePerSlot - nop["numberOfPeople__sum"]
+    return seats_left
 
 # def dateForNumSeats(num):
 #     # Return which date is available for that number of seats
 
 if __name__ == "__main__":
-    numSeatsForDate("2023-03-03")
+    numSeatsForDate("2023-01-06")
