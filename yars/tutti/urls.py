0fd361029655e6d8701c51e786c449672df577b1
Wanrong Gao <2795734G@student.gla.ac.uk>
2023-03-17 09:28:57 +0000

    add

--- yars/tutti/urls.py
+++ yars/tutti/urls.py
@@ -7,10 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('show_bookings/', views.show_bookings, name='show_bookings'),
     path('reviews/', views.reviews, name='reviews'),
-    path('booking/', views.booking, name='booking'),
-    path('booking/date-and-time', views.booking_date_and_time, name='booking_date_time'),
-    path('booking/confirmation', views.booking_confirmation, name='booking_confirmation'),
-    path('booking/completed', views.booking_completed, name='booking_completed'),
-    path('menu/', views.booking_completed, name='booking_completed'),
+
 
 ]
