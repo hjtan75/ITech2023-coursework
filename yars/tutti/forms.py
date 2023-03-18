458e9ef965b39450b5b8327b88204edea52b8ca1
redchicken75 <flyingnyancat7@gmail.com>
2023-03-18 22:09:24 +0000

    Addeding registration

--- yars/tutti/forms.py
+++ yars/tutti/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from tutti.models import Review, Booking
+from tutti.models import Review, Booking, UserProfile
+from django.contrib.auth.models import User
 
 numOfPeopleChoices = (
     ("1", "One"),
@@ -10,9 +11,36 @@ numOfPeopleChoices = (
     ("6", "Six"),
 )
 
-class numPeopleForm(forms.ModelForm):
-    numofPeople = forms.ChoiceField(choices = numOfPeopleChoices)
+# class BookingForm(forms.ModelForm):
+#     numberOfPeople = forms.ChoiceField(choices = numOfPeopleChoices) 
+#     notes = forms.CharField(max_length=1000)
+
+#     # bookingID = 
+#     # user = 
+#     date = forms.DateField()
+#     # time = 
+#     numberOfPeople = forms.ChoiceField(choices = numOfPeopleChoices) 
+#     notes = forms.CharField(max_length=1000)
+#     # bookingStatus = 
+
+#     class Meta:
+#         model = Booking,
+
 
 
 # class dateTimeForm(forms.Modelform):
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('phoneNum', 'address', 'gender',)
     
--- yars/tutti/views.py
+++ yars/tutti/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from tutti.models import User, Booking
-from tutti.forms import numPeopleForm
+from tutti.models import User, Booking, UserProfile
+from tutti.forms import UserForm, UserProfileForm
+# from tutti.forms import numPeopleForm
 # Create your views here.
 def index(request):
     return HttpResponse("Rango says hey there partner!")
@@ -31,45 +32,72 @@ def show_bookings(request):
     return render(request, 'tutti/show_bookings.html', context=context_dict)
 
 
-def booking(request):
-    context_dict = {}
+# def booking(request):
+#     context_dict = {}
 
-    form = numPeopleForm()
-    context_dict['form'] = form
+#     form = numPeopleForm()
+#     context_dict['form'] = form
 
-    # if request.method == 'POST':
-    #     form = numPeopleForm(request.POST)
+#     # if request.method == 'POST':
+#     #     form = numPeopleForm(request.POST)
 
-    # if form.is_valid():
-    #     # Save the new category and return to index page
-    #     print(form)
-    #     return redirect('tutti/booking_date_time.html')
-    # else:
-    #     print(form.errors)
+#     # if form.is_valid():
+#     #     # Save the new category and return to index page
+#     #     print(form)
+#     #     return redirect('tutti/booking_date_time.html')
+#     # else:
+#     #     print(form.errors)
 
-    # Loading the form
-    print(form)
-    return render(request, 'tutti/booking_num_people.html', context=context_dict)
+#     # Loading the form
+#     print(form)
+#     return render(request, 'tutti/booking_num_people.html', context=context_dict)
 
-def booking_date_and_time(request):
-    context_dict = {}
-    context_dict['months'] = ['June', 'July', 'August', 'September']
-    context_dict['days'] = ['01', '02', '05', '06']
-    context_dict['times'] = ['0500', '0900', '1230', '1500']
+# def booking_date_and_time(request):
+#     context_dict = {}
+#     context_dict['months'] = ['June', 'July', 'August', 'September']
+#     context_dict['days'] = ['01', '02', '05', '06']
+#     context_dict['times'] = ['0500', '0900', '1230', '1500']
 
-    return render(request, 'tutti/booking_date_time.html', context=context_dict)
+#     return render(request, 'tutti/booking_date_time.html', context=context_dict)
 
 
-def booking_confirmation(request):
-    context_dict = {}
-    context_dict['user'] = 'Adam Smith'
-    context_dict['phone'] = '07465898556'
-    context_dict['email'] = 'testing@testing.com'
-    context_dict['datatime'] = '19:30 26 February 2022'
-    context_dict['numOfPeoples'] = 5
+# def booking_confirmation(request):
+#     context_dict = {}
+#     context_dict['user'] = 'Adam Smith'
+#     context_dict['phone'] = '07465898556'
+#     context_dict['email'] = 'testing@testing.com'
+#     context_dict['datatime'] = '19:30 26 February 2022'
+#     context_dict['numOfPeoples'] = 5
 
-    return render(request, 'tutti/booking_confirmation.html', context=context_dict)
+#     return render(request, 'tutti/booking_confirmation.html', context=context_dict)
 
-def booking_completed(request):
-    return render(request, 'tutti/booking_completed.html')
+# def booking_completed(request):
+#     return render(request, 'tutti/booking_completed.html')
 
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'tutti/register.html', context={'user_form': user_form,
+                                                           'profile_form': profile_form,
+                                                           'registered': registered})
